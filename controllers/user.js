15e05ecd367ab8a94ef27640b2bcf38c3286d7a8
Alex <climberfields@gmail.com>
2018-10-21 17:56:37 -0500

    show profile page works

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require('../models/user')
 const Ex = require('../models/ex')
 
+
+//<--- Sign in page --->
 router.get('/', (req, res) => {
     User.find({}, (err, foundUser) => {
         console.log("hey")
@@ -12,18 +14,26 @@ router.get('/', (req, res) => {
     });
 });
 
-
+//<--- show new page --->
 router.get('/new', (req, res) => {
     res.render('./user/new.ejs')
-    });
+});
 
-
+//<--- After creating new user this redirects --->
 router.post('/', (req, res) => {
-    User.create(req.body, (err, foundUser)=> {
+    User.create(req.body, (err, foundUser) => {
         res.redirect('/user')
     });
 });
 
+//<--- Show route --->
+router.get('/:index', (req, res) => {
+    User.findById(req.params.index, (err, foundUser) => {
+        res.render('./user/show.ejs', {
+            user: foundUser
+        });
+    });
+});
 
 
 
