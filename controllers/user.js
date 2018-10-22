6ff3a2d3824001b08198d46a0b9cdf66c48efc15
Alex <climberfields@gmail.com>
2018-10-22 12:40:26 -0500

    delete async functioning

--- controllers/user.js
+++ controllers/user.js
@@ -6,15 +6,15 @@ const Ex = require('../models/ex')
 
 //<--- Sign in page --->
 router.get('/', async (req, res) => {
-try {
-    const newUser = await User.find();
-    res.render('./user/index.ejs', {
-        user: newUser
-    });
-} catch (err) {
-    res.send(err)
-    //Catch Statement
-}
+    try {
+        const newUser = await User.find();
+        res.render('./user/index.ejs', {
+            user: newUser
+        });
+    } catch (err) {
+        res.send(err)
+        //Catch Statement
+    }
 });
 
 //<--- show new page --->
@@ -25,10 +25,9 @@ router.get('/new', (req, res) => {
 //<--- After creating new user this redirects --->
 router.post('/', async (req, res) => {
     try {
-await User.create(req.params.id) 
-            res.redirect('/user', {
-        });
-    } catch(err) {
+        await User.create(req.params.id)
+        res.redirect('/user', {});
+    } catch (err) {
         res.send(err)
         //Catch Statement
     }
@@ -44,28 +43,34 @@ router.get('/:index', (req, res) => {
 });
 
 //<--- edit user --->
-router.get('/:index/edit', (req, res) => {
-    User.findById(req.params.index, (err, foundUser) => {
-        console.log("edit user works")
-        res.render('./user/edit.ejs', {
-            user: foundUser
-        });
+router.get('/:index/edit', async (req, res) => {
+        try { 
+            const userEdit = await User.findByID(req.params.index)
+            res.render('./user/edit.ejs', {
+                user: userEdit
+            });
+        } catch (err) {
+            res.send(err)
+            //Catch Statement
+        }
     });
-});
 
 router.put('/:index', (req, res) => {
     console.log(req.body);
     User.findByIdAndUpdate(req.params.index, req.body, (err, foundUser) => {
         res.redirect('/');
     });
-
 });
 //<--- delete user --->
 
-router.delete('/:index', (req, res) => {
-    User.findByIdAndRemove(req.params.index, (err, foundUser) => {
-        res.redirect('/user/')
-    });
+router.delete('/:index', async (req, res) => {
+    try {
+       await User.findByIdAndRemove(req.params.index)
+            res.redirect('/user/')
+    } catch (err) {
+    res.send(err)
+        //Catch Statement
+    }
 });
 
 
