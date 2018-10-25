94cf10f9fbbc62f3e95f7e2958d35a0a62f3a1c0
Alex <climberfields@gmail.com>
2018-10-25 16:48:41 -0500

    fixed nav bar, login page, took sex out of registration.

--- server.js
+++ server.js
@@ -9,18 +9,23 @@ const exController   = require('./controllers/ex');
 const userController = require('./controllers/user');
 const loginController = require('./controllers/login');
 
+//static styles 
+app.use(express.static('public'));
+
+
+
 //middleware
 app.use(session({
     secret: 'This is some random secret string',
     resave: false,
     saveUninitialized: false
 }));
-
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use('/ex', exController);
 app.use('/user', userController);
 app.use('/login', loginController);
+
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
